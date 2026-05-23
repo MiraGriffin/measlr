377c8289eb15b57b77eaabc5fb07c8c955294fd8
Mira Griffin <mgriff35@calpoly.edu>
2026-05-22 17:30:09 -0700

    package plans

---
+++ R/load_data.R
@@ -0,0 +1,13 @@
+#' Load measles data from a CSV URL
+#'
+#' This function downloads and reads the measles data sets used in the analysis.
+#'
+#' @return A tibble with the loaded dataset.
+#'
+#' @importFrom readr read_csv
+#' @export
+load_data <- function(){
+  readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-24/cases_month.csv')
+  readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-24/cases_year.csv')
+
+}
