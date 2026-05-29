7249798f74eb4bf5f6ee047452eee962d8d56385
Mira Griffin <mgriff35@calpoly.edu>
2026-05-29 16:13:31 -0700

    test for plot

--- tests/testthat/test-plot-test.R
+++ tests/testthat/test-plot-test.R
@@ -1,3 +1,22 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("mean_measles_by_country returns a ggplot", {
+  data_urban   <- urban_data
+  countries   <- c("United States", "India")
+  country <- "United States"
+  wrong_countries <- c("United States", "Denver")
+
+  # Tests that a vector of countries works
+  multiple_countries <- mean_measles_by_country(dat_urban, countries)
+
+  #Tests that an input of a single country works
+  one_country <- mean_measles_by_country(dat_urban, countries)
+
+  expect_s3_class(multiple_countries, "ggplot")
+  expect_s3_class(one_countries, "ggplot")
+
+  # Tests that an error is thrown if at least one of the countries inputted is
+  # not found in the dataset.
+  expect_error(
+    mean_measles_by_country(measles_data, urban_data, wrong_countries),
+    "One or more countries not found in dataset"
+  )
 })
