130b7b01ecdef1ca1bbd92331219d8900e8aeacb
Mira Griffin <mgriff35@calpoly.edu>
2026-06-06 17:31:26 -0700

    fixed test

--- tests/testthat/test-summary-urban-test.R
+++ tests/testthat/test-summary-urban-test.R
@@ -11,7 +11,15 @@ test_that("output is a data frame", {
 
   expect_equal(result$country[1], "China")
   expect_equal(result$month[1], 1)
-  expect_equal(result$mean_measles_incidence[1], round(729.714, 3))
-  expect_equal(result$sd_measles_incidence[1], round(922.900, 3))
+  expect_equal(
+    result$mean_measles_incidence[1],
+    729.714,
+    tolerance = 1e-3
+  )
+  expect_equal(
+    result$sd_measles_incidence[1],
+    922.900,
+    tolerance = 1e-3
+  )
 })
 
